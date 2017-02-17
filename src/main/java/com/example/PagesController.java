2c19c2c973baaa62fed291d46a772b96619dc8df
Stephanie Ogburn <stephanie.ogburn@gmail.com>
2017-02-16 19:36:39 -0700

    initial commit

---
+++ src/main/java/com/example/PagesController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Created by stephaniepaigeogburn on 2/16/17.
+ */
+@RestController
+public class PagesController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+}
